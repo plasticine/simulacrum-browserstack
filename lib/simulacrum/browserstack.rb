9679e0ba2ff73399b4c1a1fbf0c673aa2bad4016
Justin Morris <desk@pixelbloom.com>
2014-08-17 09:24:01 +1000

    Gem init.

---
+++ lib/simulacrum/browserstack.rb
@@ -0,0 +1,7 @@
+require "simulacrum/browserstack/version"
+
+module Simulacrum
+  module Browserstack
+    # Your code goes here...
+  end
+end
---
+++ lib/simulacrum/browserstack/version.rb
@@ -0,0 +1,5 @@
+module Simulacrum
+  module Browserstack
+    VERSION = "0.0.1"
+  end
+end
